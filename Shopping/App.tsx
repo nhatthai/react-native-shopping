b462f3a6861caa15b858131d9ed2f69b9c354e33
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2021-10-23 09:36:47 +0700

    Add components

--- Shopping/App.tsx
+++ Shopping/App.tsx
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import ProductContext from './hooks/productContext';
+import { productReducer, productState } from './hooks/productReducer';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const [state, dispatch] = useReducer(productReducer, productState);
+  const [colorScheme, setColorScheme] = useState("light");
+
+  useEffect(() => {
+    if(state.isDarkMode) {
+      setColorScheme("dark");
+    }
+    else {
+      setColorScheme("light");
+    }
+  }, [state.isDarkMode])
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
+        <ProductContext.Provider value={{ state, dispatch }}>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
+        </ProductContext.Provider>
       </SafeAreaProvider>
     );
   }
---
+++ Shopping/components/Separator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Separator () {
+  return (
+    <View style={{ borderBottomWidth: 0.5, borderBottomColor: '#a9a9a9' }} />
+  )
+}
